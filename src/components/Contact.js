553b6b56f67ba58b36deb3c01c7c47f827803744
Kenneth Boelter <kennethboelter@pop-os.localdomain>
2020-09-28 19:32:56 -0600

    progress

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export default function Contact() {
     return (
-        <div>
-            <h1>HELLO WORLD</h1>
-        </div>
+        <Container>
+            <p>Email: krboelter@gmail.com</p>
+        </Container>
     )
 }
+
+const Container = styled.div`
+	display: block;
+	float; left;
+    width: 950px;
+    height: 500px;
+	margin-left: 10rem;
+    border-radius: 5px;
+    background-image: linear-gradient(to right, black, 85%, rgba(0, 0, 0, 0));
+`
